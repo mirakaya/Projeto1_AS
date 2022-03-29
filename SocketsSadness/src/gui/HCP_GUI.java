88c1d959750477c25c829495d8098275c96fbe26
mirakaya <59024914+mirakaya@users.noreply.github.com>
2022-03-29 05:49:49 -0600

    Better naming and clean up

--- SocketsSadness/src/gui/HCP_GUI.java
+++ SocketsSadness/src/gui/HCP_GUI.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-
 import static java.awt.Color.*;
-
 
 public class HCP_GUI extends JDialog {
 
@@ -11,10 +9,7 @@ public class HCP_GUI extends JDialog {
     * put_patient -> puts 1 patient in their hall and redraws the GUI
     * remove_patient -> removes 1 patient from the hall and redraws the GUI
     *
-    * nurses - needs redoing because nurses are no longer part of the rooms
-    *
     * */
-
 
     private JPanel contentPane;
     private JPanel hospital;
@@ -58,17 +53,18 @@ public class HCP_GUI extends JDialog {
         setContentPane(contentPane);
         setModal(true);
 
-        draw_person(patients_MDR1);
-        draw_person(patients_MDR2);
-        draw_person(patients_MDR3);
-        draw_person(patients_MDR4);
+        //draw rooms with fixed professionals (doctors)
+        find_list_to_draw(patients_MDR1);
+        find_list_to_draw(patients_MDR2);
+        find_list_to_draw(patients_MDR3);
+        find_list_to_draw(patients_MDR4);
 
 
         //test
         put_patient("ETR1", "C1", "Gray");
         put_patient("ETR2", "A4", "Gray");
         put_patient("ETR1", "C2", "Yellow");
-        put_patient("BEN", "A4", "Gray");
+        put_patient("BEN", "A1", "Gray");
         put_patient("AEX", "C5", "Red");
 
         put_patient("EVR1", "Nurse EVR1", "");
@@ -82,7 +78,7 @@ public class HCP_GUI extends JDialog {
 
     }
 
-    public void remove_patient_aux (String [][] patient_list, String id) {
+    private void find_remove(String [][] patient_list, String id) {
         for (int pos = 0; pos < patient_list.length; pos ++){
 
             if (patient_list[pos][0] == id){
@@ -92,7 +88,7 @@ public class HCP_GUI extends JDialog {
             }
         }
 
-        draw_person(patient_list);
+        find_list_to_draw(patient_list);
 
     }
 
@@ -100,66 +96,66 @@ public class HCP_GUI extends JDialog {
 
         switch(hall) {
             case "BEN":
-                remove_patient_aux(patients_b_entering, id);
+                find_remove(patients_b_entering, id);
                 break;
 
             case "ETR1":
-                remove_patient_aux(patients_ETR1, id);
+                find_remove(patients_ETR1, id);
                 break;
 
             case "ETR2":
-                remove_patient_aux(patients_ETR2, id);
+                find_remove(patients_ETR2, id);
                 break;
 
             case "EVR1":
-                remove_patient_aux(patients_EVR1, id);
+                find_remove(patients_EVR1, id);
                 break;
 
             case "EVR2":
-                remove_patient_aux(patients_EVR2, id);
+                find_remove(patients_EVR2, id);
                 break;
 
             case "EVR3":
-                remove_patient_aux(patients_EVR3, id);
+                find_remove(patients_EVR3, id);
                 break;
 
             case "EVR4":
-                remove_patient_aux(patients_EVR4, id);
+                find_remove(patients_EVR4, id);
                 break;
 
             case "WTH1":
-                remove_patient_aux(patients_WTH1, id);
+                find_remove(patients_WTH1, id);
                 break;
 
             case "WTH2":
-                remove_patient_aux(patients_WTH2, id);
+                find_remove(patients_WTH2, id);
                 break;
 
             case "MDW":
-                remove_patient_aux(patients_MDW, id);
+                find_remove(patients_MDW, id);
                 break;
 
             case "MDR1":
-                remove_patient_aux(patients_MDR1, id);
+                find_remove(patients_MDR1, id);
                 break;
 
             case "MDR2":
-                remove_patient_aux(patients_MDR2, id);
+                find_remove(patients_MDR2, id);
                 break;
 
             case "MDR3":
-                remove_patient_aux(patients_MDR3, id);
+                find_remove(patients_MDR3, id);
                 break;
 
             case "MDR4":
-                remove_patient_aux(patients_MDR4, id);
+                find_remove(patients_MDR4, id);
                 break;
 
             case "PYH":
-                remove_patient_aux(patients_PYH, id);
+                find_remove(patients_PYH, id);
                 break;
             case "AEX":
-                remove_patient_aux(patients_a_exiting, id);
+                find_remove(patients_a_exiting, id);
                 break;
 
             default:
@@ -169,7 +165,7 @@ public class HCP_GUI extends JDialog {
 
     }
 
-    private void put_patient_aux (String [][] patients_list, String id, String color){ //TODO: rename
+    private void add_patient_to_list (String [][] patients_list, String id, String color){ //TODO: rename
 
         Boolean bool_patient_added = false;
 
@@ -187,79 +183,75 @@ public class HCP_GUI extends JDialog {
 
         }
 
-        draw_person(patients_list);
+        find_list_to_draw(patients_list);
 
     }
 
     public void put_patient(String hall, String id, String color){
 
-        Boolean bool_patient_added = false;
-
-
-
         switch(hall) {
             case "BEN":
-                put_patient_aux(patients_b_entering, id, color);
+                add_patient_to_list(patients_b_entering, id, color);
                 break;
 
             case "ETR1":
-                put_patient_aux(patients_ETR1, id, color);
+                add_patient_to_list(patients_ETR1, id, color);
                 break;
 
             case "ETR2":
-                put_patient_aux(patients_ETR2, id, color);
+                add_patient_to_list(patients_ETR2, id, color);
                 break;
 
             case "EVR1":
-                put_patient_aux(patients_EVR1, id, color);
+                add_patient_to_list(patients_EVR1, id, color);
                 break;
 
             case "EVR2":
-                put_patient_aux(patients_EVR2, id, color);
+                add_patient_to_list(patients_EVR2, id, color);
                 break;
 
             case "EVR3":
-                put_patient_aux(patients_EVR3, id, color);
+                add_patient_to_list(patients_EVR3, id, color);
                 break;
 
             case "EVR4":
-                put_patient_aux(patients_EVR4, id, color);
+                add_patient_to_list(patients_EVR4, id, color);
                 break;
 
             case "WTH1":
-                put_patient_aux(patients_WTH1, id, color);
+                add_patient_to_list(patients_WTH1, id, color);
                 break;
 
             case "WTH2":
-                put_patient_aux(patients_WTH2, id, color);
+                add_patient_to_list(patients_WTH2, id, color);
                 break;
 
             case "MDW":
-                put_patient_aux(patients_MDW, id, color);
+                add_patient_to_list(patients_MDW, id, color);
                 break;
 
             case "MDR1":
-                put_patient_aux(patients_MDR1, id, color);
+                add_patient_to_list(patients_MDR1, id, color);
                 break;
 
             case "MDR2":
-                put_patient_aux(patients_MDR2, id, color);
+                add_patient_to_list(patients_MDR2, id, color);
                 break;
 
             case "MDR3":
-                put_patient_aux(patients_MDR3, id, color);
+                add_patient_to_list(patients_MDR3, id, color);
                 break;
 
             case "MDR4":
-                put_patient_aux(patients_MDR4, id, color);
+                add_patient_to_list(patients_MDR4, id, color);
                 break;
 
             case "PYH":
-                put_patient_aux(patients_PYH, id, color);
+                add_patient_to_list(patients_PYH, id, color);
                 break;
 
             case "AEX":
-                put_patient_aux(patients_a_exiting, id, color);
+                add_patient_to_list(patients_a_exiting, id, color);
                 break;
 
             default:
@@ -269,7 +261,7 @@ public class HCP_GUI extends JDialog {
 
 
 
-    private void gui_aux (JPanel curr_panel, String [][] patients_list){ //TODO: rename
+    private void erase_and_draw(JPanel curr_panel, String [][] patients_list){ //TODO: rename
         curr_panel.removeAll();
 
         for (int patient = 0; patient < patients_list.length; patient++){
@@ -281,49 +273,49 @@ public class HCP_GUI extends JDialog {
 
     }
 
-    public void draw_person(String[][] list){
+    private void find_list_to_draw(String[][] list){
         String[] doctor;
 
         if (list == patients_b_entering) {
-            gui_aux(b_entering, patients_b_entering);
+            erase_and_draw(b_entering, patients_b_entering);
         }else if (list == patients_ETR1) {
-            gui_aux(ETR1, patients_ETR1);
+            erase_and_draw(ETR1, patients_ETR1);
         }else if (list == patients_ETR2) {
-                gui_aux(ETR2, patients_ETR2);
+                erase_and_draw(ETR2, patients_ETR2);
         }else if (list == patients_EVR1) {
-                gui_aux(EVR1, patients_EVR1);
+                erase_and_draw(EVR1, patients_EVR1);
         }else if (list == patients_EVR2) {
-                gui_aux(EVR2, patients_EVR2);
+                erase_and_draw(EVR2, patients_EVR2);
         }else if (list == patients_EVR3) {
-                gui_aux(EVR3, patients_EVR3);
+                erase_and_draw(EVR3, patients_EVR3);
         }else if (list == patients_EVR4) {
-                gui_aux(EVR4, patients_EVR4);
+                erase_and_draw(EVR4, patients_EVR4);
         }else if (list == patients_WTH1) {
-                gui_aux(WTH1, patients_WTH1);
+                erase_and_draw(WTH1, patients_WTH1);
         }else if (list == patients_WTH2) {
-                gui_aux(WTH2, patients_WTH2);
+                erase_and_draw(WTH2, patients_WTH2);
         }else if (list == patients_MDW) {
-                gui_aux(MDW, patients_MDW);
+                erase_and_draw(MDW, patients_MDW);
         }else if (list == patients_MDR1) {
-                gui_aux(MDR1, patients_MDR1);
+                erase_and_draw(MDR1, patients_MDR1);
                 doctor = new String[]{"Doctor MDR1", " "};
                 draw_human(MDR1, doctor);
         }else if (list == patients_MDR2) {
-                gui_aux(MDR2, patients_MDR2);
+                erase_and_draw(MDR2, patients_MDR2);
                 doctor = new String[]{"Doctor MDR2", " "};
                 draw_human(MDR2, doctor);
         }else if (list == patients_MDR3) {
-                gui_aux(MDR3, patients_MDR3);
+                erase_and_draw(MDR3, patients_MDR3);
                 doctor = new String[]{"Doctor MDR3", " "};
                 draw_human(MDR3, doctor);
         }else if (list == patients_MDR4) {
-            gui_aux(MDR4, patients_MDR4);
+            erase_and_draw(MDR4, patients_MDR4);
             doctor = new String[]{"Doctor MDR4", " "};
             draw_human(MDR4, doctor);
         }else if (list == patients_PYH) {
-            gui_aux(PYH, patients_PYH);
+            erase_and_draw(PYH, patients_PYH);
         } else if (list == patients_a_exiting) {
-            gui_aux(a_exiting, patients_a_exiting);
+            erase_and_draw(a_exiting, patients_a_exiting);
         }else {
             System.out.println("Algo de errado não está certo...");
         }
@@ -332,7 +324,7 @@ public class HCP_GUI extends JDialog {
     }
 
 
-    public void draw_human(JPanel curr_frame, String[] patient){//TODO: rename
+    private void draw_human(JPanel curr_frame, String[] patient){//TODO: rename
 
         curr_frame.setLayout(new FlowLayout());
         curr_frame.add(new JLabel(patient[0]));
