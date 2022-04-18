537a1d2b7a9811578c12e7fd3d816d2aef6a77df
José Gonçalves <jpsextafeira@gmail.com>
2022-04-18 15:59:46 +0100

    Full implementation of the simulation controller
    It includes all methods the shared regions need to log, update gui and test if the simulation is running or suspended.

--- ASProject1/src/HCP/Monitors/Simulation/MSimulationController.java
+++ ASProject1/src/HCP/Monitors/Simulation/MSimulationController.java
@@ -1,13 +1,24 @@
 package HCP.Monitors.Simulation;
 
+import HCP.Enums.AvailableHalls;
+import HCP.Monitors.MLogger;
+import HCP.gui.HCP_GUI;
+
+import java.awt.event.WindowEvent;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MSimulationController implements ISCEntities, ISCSimulation {
     private final ReentrantLock monitor = new ReentrantLock();
+    private final HCP_GUI gui = new HCP_GUI();
+    private final MLogger logger;
 
     private boolean isPaused = false;
     private final Condition resumed = monitor.newCondition();
+
+    public MSimulationController(int patientsCount) {
+        logger = new MLogger(patientsCount);
+    }
 
     @Override
     public void waitIfPaused() throws InterruptedException {
@@ -31,5 +42,21 @@ public class MSimulationController implements ISCEntities, ISCSimulation {
         isPaused = false;
         resumed.signalAll();
         monitor.unlock();
+    }
+
+    public void log(String patientId, AvailableHalls room) throws InterruptedException {
+        logger.createContent(patientId, room);
+    }
+
+    public void putPatient(String hall, String id, String color) throws InterruptedException {
+        gui.put_patient(hall, id, color);
+    }
+
+    public void removePatient(String hall, String id) throws InterruptedException {
+        gui.remove_patient(hall, id);
+    }
+
+    public void cleanup() {
+        gui.dispatchEvent(new WindowEvent(gui, WindowEvent.WINDOW_CLOSING));
     }
 }
